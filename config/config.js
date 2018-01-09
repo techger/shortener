f2f4309434d5d328e0c36677768e3c9c99d2afc7
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-01-09 02:49:38 +0000

    updated

--- config/config.js
+++ config/config.js
@@ -1,9 +1,5 @@
 var config = {};
 
-config.db = {};
-config.webhost = 'http://localhost:3000/';
-
-config.db.host = 'localhost';
-config.db.name = 'url_shortener';
+config.webhost = 'https://techstar-url-shortener-tortuvshin.c9users.io/';
 
 module.exports = config;
